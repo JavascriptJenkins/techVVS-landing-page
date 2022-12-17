acebdef0147bb54fe43b74ebbcad7bb4454dd5b8
odinarussell@gmail.com <odinarussel@gmail.com>
2022-12-16 19:29:33 -0600

    added images & favicon

--- example-model/src/components/about/About.jsx
+++ example-model/src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import AboutImg from "../../assets/about.jpg";
+import AboutImg from "../../assets/pexels-olia-danilevich-4974914.jpg";
 import CV from "../../assets/Smith-Cv.pdf";
 import Info from "./Info";
 
